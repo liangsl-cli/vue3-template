3d984c6e9b50138fe3096e1b977a5f27e2828ff1
liangshenglin <641579182@qq.com>
2022-02-14 16:32:39 +0800

    feat: 新增统一异常处理

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 import '@/styles/index.scss'
 
+import { setupErrorHandle } from '@/utils/error-handle';
+
 const app = createApp(App)
+setupErrorHandle(app); // 统一异常处理
 
 app.mount('#app')
